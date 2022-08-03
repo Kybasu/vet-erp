899b3ae2e8894860c4395bae494d8a8cd5274f2e
Flavien Chagras <flavien.chagras@protonmail.com>
2022-08-03 18:32:07 +0200

    feat(db): variant migration added

---
+++ database/migrations/2022_07_24_174610_create_variants_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('variants', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('article_id');
+            $table->string('default_code');
+            $table->unsignedFloat('price');
+            $table->unsignedFloat('weight');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('article_id')->references('id')->on('articles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('variants');
+    }
+};
